2e2a73878ff3b7bcfb468ab20a167462d7df9041
RonyShir24 <127685032+RonyShir24@users.noreply.github.com>
2023-05-31 21:38:17 +0300

    Update recipes.js

--- routes/recipes.js
+++ routes/recipes.js
@@ -4,6 +4,41 @@ const recipes_utils = require("./utils/recipes_utils");
 
 router.get("/", (req, res) => res.send("im here"));
 
+router.get("/search/:query&:number&:cuisine&:diet&:intolerances&:sort", async (req, res, next) => {
+  try {
+    const recipe = await recipes_utils.searchRecipe(req.params.query,req.params.number,req.params.cuisine,req.params.diet,req.params.intolerances,req.params.sort);
+    let recipes_array = [];
+    recipe.data["results"].map((element) => recipes_array.push(element["id"])); //extracting the recipe ids into array
+    const results = await recipes_utils.getRecipessearchbyID(recipes_array);
+    res.status(200).send(results);
+  } catch (error) {
+    next(error);
+  }
+  //recipes_utils.getSearchResult(element))
+});
+
+
+router.get("/getRandomRecipes", async (req, res, next) => {
+  try {
+    const recipe = await recipes_utils.getRandomRecipes();
+    let recipes_array = [];
+    recipe.data["recipes"].map((element) => recipes_array.push(recipes_utils.getRecipeDetailsRandom(element))); //extracting the recipe ids into array
+    res.send(recipes_array);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/getFullRecipe/:recipeId", async (req, res, next) => {
+  try {
+    const recipe = await recipes_utils.getFullRecipeDetails(req.params.recipeId);
+    res.send(recipe);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
 
 /**
  * This path returns a full details of a recipe by its id
@@ -17,16 +52,7 @@ router.get("/:recipeId", async (req, res, next) => {
   }
 });
 
-router.get("/recipe/getRandomRecipes", async (req, res, next) => {
-  try {
-    const recipe = await recipes_utils.getRandomRecipes();
-    let recipes_array = [];
-    recipe.map((element) => recipes_array.push(recipes_utils.getRecipeDetailsRandom(element))); //extracting the recipe ids into array
-    res.send(recipes_array);
-  } catch (error) {
-    next(error);
-  }
-});
+
 
 
 module.exports = router;
