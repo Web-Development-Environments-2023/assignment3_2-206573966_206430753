0cbb4d6561fe6b989596037e1a3a9ee4eee9097f
RonyShir24 <127685032+RonyShir24@users.noreply.github.com>
2023-06-25 01:04:25 +0300

    Update user_utils.js

--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -1,7 +1,16 @@
 const DButils = require("./DButils");
 
 async function markAsFavorite(user_id, recipe_id){
+    // const recipes_id = await DButils.execQuery(`select recipe_id from MyFavorites where user_id=${user_id}`);
+    // let array_id=[];
+    // for (let i = 0; i < recipes_id.length; i++) {
+    //     array_id[i] =recipes_id[i].recipe_id
+    //   }
+    // console.log(array_id);
+    // if(!array_id.includes(recipe_id))
+    // {
     await DButils.execQuery(`insert into MyFavorites (user_id,recipe_id) values (${user_id},${recipe_id})`);
+    // }
 }
 
 async function markAsViewed(user_id, recipe_id){
@@ -35,6 +44,11 @@ async function getMyFamilyRecipes(user_id)
     return recipes_id;
 }
 
+async function getMyFamilyFullRecipes(recipe_id)
+{
+    const recipes_id = await DButils.execQuery(`select * from MyFamilyRecipes where id=${recipe_id}`);
+    return recipes_id;
+}
 
 async function getMyRecipeInformation(recipe_id) {
     const recipe_information = await DButils.execQuery(`select * from MyRecipes where id='${recipe_id}'`);
@@ -57,6 +71,48 @@ async function getMyRecipePreviewDetails(recipe_id) {
     return value;
 }
 
+async function getMyFullRecipesPreview(recipe_id) {
+    let recipe_info = await getMyRecipeInformation(recipe_id);
+    let { id , recipe_name, recipe_time,popularity, vegan,gloten,courses_num, instructions, ingredients, vegetarian, img} = recipe_info[0];
+    value={
+        id: id,
+        recipe_name: recipe_name,
+        recipe_time: recipe_time,
+        popularity: popularity,
+        vegan: vegan,
+        gloten: gloten,
+        vegetarian: vegetarian,
+        courses_num:courses_num,
+        instructions:instructions,
+        ingridiants:ingredients,
+        img: img,
+    };
+    return value;
+}
+
+
+async function getMyFullFamilyRecipesPreview(recipe_id) {
+    let recipe_info = await getMyFamilyFullRecipes(recipe_id);
+    let { id , recipe_name, recipe_time,popularity, vegan,gloten,courses_num, instructions, ingredients, vegetarian, img,event,maker} = recipe_info[0];
+    value={
+        id: id,
+        recipe_name: recipe_name,
+        recipe_time: recipe_time,
+        popularity: popularity,
+        vegan: vegan,
+        gloten: gloten,
+        vegetarian: vegetarian,
+        courses_num:courses_num,
+        instructions:instructions,
+        ingridiants:ingredients,
+        img: img,
+        event:event,
+        maker:maker,
+    };
+    return value;
+}
+
+
 async function getMyRecipesPreview(recipe_ids_list){
     return_array = []
     for (let i = 0; i < recipe_ids_list.length; i++) {
@@ -72,7 +128,7 @@ async function getMyFamilyRecipeInformation(recipe_id) {
 
 async function getMyFamilyRecipePreviewDetails(recipe_id) {
     let recipe_info = await getMyFamilyRecipeInformation(recipe_id);
-    let { id ,user_id, recipe_name, recipe_time,popularity, vegan,gloten,courses_num, instructions, ingridiants, vegetarian, img} = recipe_info[0];
+    let { id ,user_id, recipe_name, recipe_time,popularity, vegan,gloten,courses_num, instructions, ingridiants, vegetarian, img,event,maker} = recipe_info[0];
     value={
         id: id,
         recipe_name: recipe_name,
@@ -82,6 +138,8 @@ async function getMyFamilyRecipePreviewDetails(recipe_id) {
         gloten: gloten,
         vegetarian: vegetarian,
         img: img,
+        event:event,
+        maker:maker,
     };
     return value;
 }
@@ -95,13 +153,31 @@ async function getMyFamilyRecipesPreview(recipe_ids_list){
 }
 
 async function addtoThreelastView(user_id,recipe_id,place){
-    await DButils.execQuery(`UPDATE lastviewd SET recipe${place}=${recipe_id} WHERE user_id=${user_id}`);
-    let new_place=place+1;
-    if(new_place==4)
+    const recipes_id = await DButils.execQuery(`select recipe1,recipe2,recipe3 from lastviewd where user_id=${user_id}`);
+    let recipes_id_array=[]
+    if(recipes_id[0].recipe1!=null)
     {
-        new_place=1;
+        recipes_id_array.push(recipes_id[0].recipe1);
     }
-    await DButils.execQuery(`UPDATE lastviewd SET place=${new_place} WHERE user_id=${user_id}`);
+    if(recipes_id[0].recipe2!=null)
+    {
+        recipes_id_array.push(recipes_id[0].recipe2);
+    }
+    if(recipes_id[0].recipe3!=null)
+    {
+        recipes_id_array.push(recipes_id[0].recipe3);
+    }
+    if(!recipes_id_array.includes(recipe_id))
+    {
+        await DButils.execQuery(`UPDATE lastviewd SET recipe${place}=${recipe_id} WHERE user_id=${user_id}`);
+        let new_place=place+1;
+        if(new_place==4)
+        {
+            new_place=1;
+        }
+        await DButils.execQuery(`UPDATE lastviewd SET place=${new_place} WHERE user_id=${user_id}`);
+    }
+
   };
 
 
@@ -121,3 +197,7 @@ exports.markAsViewed=markAsViewed;
 exports.getAllViewedRecipes=getAllViewedRecipes;
 
 exports.addtoThreelastView=addtoThreelastView;
+
+exports.getMyFullRecipesPreview=getMyFullRecipesPreview;
+exports.getMyFullFamilyRecipesPreview=getMyFullFamilyRecipesPreview;
+exports.getMyFamilyFullRecipes=getMyFamilyFullRecipes;
